a1917f8b799518de1bf89a0fe1626a12159750b7
Bruno Oliniski <brunooliniski@gmail.com>
2024-08-27 14:59:22 -0300

    Global CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import { Header } from "./components/Header"
+import { Post } from "./Post"
+
+import "./global.css"
+
 export function App() {
   return (
     <>
+    <Header />
+    <Post 
+      author="Bruno Oliniski" 
+      content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem corporis vero, repellat labore animi in dolore odit ipsam sit, doloremque similique totam eaque earum eos? Minus, recusandae natus. Nihil, impedit!"
+    />
+    <Post 
+      author="Bruno Bihuna" 
+      content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem corporis vero, repellat labore animi in dolore odit ipsam sit, doloremque similique totam eaque earum eos? Minus, recusandae natus. Nihil, impedit!"
+    />
     </>
   )
 }
-
-export default App
