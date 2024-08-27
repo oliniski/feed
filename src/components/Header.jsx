4460757c6905ea24de694245c895c09ede4614fa
Bruno Oliniski <brunooliniski@gmail.com>
2024-08-27 15:15:21 -0300

    Logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
-import styles from "./Header.module.css"
+import styles from "./Header.module.css";
+
+import logo from "../assets/logo.svg";
 
 export function Header() {
   return (
     <header className={styles.header}>
-      <strong>Feed</strong>
+      <img src={logo} alt="Logotipo" />
     </header>
   )
 }
