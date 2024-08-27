609fd551f5f1ce34242f87093bb26778292dc0e4
Bruno Oliniski <brunooliniski@gmail.com>
2024-08-27 15:57:03 -0300

    Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
 import { Header } from "./components/Header"
 import { Post } from "./Post"
-
+import { Sidebar
+  
+ } from "./components/Sidebar"
+import styles from "./App.module.css"
 import "./global.css"
 
 export function App() {
   return (
     <>
     <Header />
-    <Post 
-      author="Bruno Oliniski" 
-      content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem corporis vero, repellat labore animi in dolore odit ipsam sit, doloremque similique totam eaque earum eos? Minus, recusandae natus. Nihil, impedit!"
-    />
-    <Post 
-      author="Bruno Bihuna" 
-      content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem corporis vero, repellat labore animi in dolore odit ipsam sit, doloremque similique totam eaque earum eos? Minus, recusandae natus. Nihil, impedit!"
-    />
+
+    <div className={styles.wrapper}>
+      <Sidebar />
+
+      <main>
+        <Post 
+          author="Bruno Oliniski" 
+          content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem corporis vero, repellat labore animi in dolore odit ipsam sit, doloremque similique totam eaque earum eos? Minus, recusandae natus. Nihil, impedit!"
+        />
+        <Post 
+          author="Bruno Bihuna" 
+          content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem corporis vero, repellat labore animi in dolore odit ipsam sit, doloremque similique totam eaque earum eos? Minus, recusandae natus. Nihil, impedit!"
+        />
+      </main>
+    </div>
     </>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,31 @@
+import { PencilLine } from 'phosphor-react';
+
+import styles from './Sidebar.module.css';
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img 
+        className={styles.coverImage}
+        src="https://images.unsplash.com/photo-1461749280684-dccba630e2f6?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+      />
+
+      <div className={styles.profile}>
+        <img 
+          className={styles.avatar}
+          src="https://github.com/oliniski.png"
+        />
+          <strong>Bruno Oliniski</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine
+            size={20}
+            className={styles.pencilIcon} 
+          />
+          Editar Perfil</a>
+      </footer>
+    </aside>
+  )
+}
